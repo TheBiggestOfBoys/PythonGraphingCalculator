92d5cd613b63a8e6f9bb9c557466c067e54fac80
Jake Scott <69864859+TheBiggestOfBoys@users.noreply.github.com>
2022-12-29 21:37:31 -0500

    Cleaned up files

--- GraphList.py
+++
@@ -1,8 +0,0 @@
-X = [-3, 1, 6, 0, 5, 2, 6, 7, 8, 9]
-Y = [9, -8, 7, 6, -5, -9, 3, 3, 5, 0]
-from turtle import goto
-
-print("X, Y")
-for point in range(len(X)):
-    print("(", X[point], ",", Y[point], ")")
-    goto(X[point]*20, Y[point]*20)
